921552e218bb59b8a6aa97ca5c3294571f0ab33e
belle <ehoi.loveyourself@gmail.com>
2025-02-14 22:05:41 +0900

    feat: 다시 만들었다!

---
+++ src/main/java/jpabook/jpashop/domain/items/Item.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain.items;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+// SINGLE_TABLE 해ㅑ해
+@Entity
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+}
