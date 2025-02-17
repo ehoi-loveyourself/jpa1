04e9b638e1e506389f2cedcc1dc322c6328fbe6d
belle <ehoi.loveyourself@gmail.com>
2025-02-17 21:57:32 +0900

    feat: MemberService 작성

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 검증
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        List<Member> members = memberRepository.findByName(member.getName());
+        if (!members.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 회원 조회
+     */
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
